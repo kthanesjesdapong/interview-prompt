356ba3f91325c515823e68ac92a64fce961518e4
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2024-07-26 01:00:33 -0700

    chore(deps): add axios

--- vite-config/base.config.ts
+++ vite-config/base.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
-export default defineConfig({
-  base: './',
-  envPrefix: 'CLIENT_',
-  plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.woff', '**/*.woff2', '**.*.ttf', '**.*.JPG'],
-  resolve: {
-    alias: {
-      '@app': resolve(__dirname, '../src/app'),
-      '@features': resolve(__dirname, '../src/features'),
-      '@pages': resolve(__dirname, '../src/pages'),
-      '@components': resolve(__dirname, '../src/shared/components'),
-      '@ui': resolve(__dirname, '../src/shared/ui'),
-      '@api': resolve(__dirname, '../src/shared/api'),
-      '@lib': resolve(__dirname, '../src/shared/lib'),
-      '@utils': resolve(__dirname, '../src/shared/utils')
-    }
+
+export default (({ mode }: { mode: UserConfig['mode']; }) => {
+  if (mode === 'development') {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   }
+  return defineConfig({
+    base: './',
+    envPrefix: 'CLIENT_',
+    plugins: [react()],
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.woff', '**/*.woff2', '**.*.ttf', '**.*.JPG'],
+    resolve: {
+      alias: {
+        '@app': resolve(__dirname, '../src/app'),
+        '@features': resolve(__dirname, '../src/features'),
+        '@pages': resolve(__dirname, '../src/pages'),
+        '@components': resolve(__dirname, '../src/shared/components'),
+        '@ui': resolve(__dirname, '../src/shared/ui'),
+        '@api': resolve(__dirname, '../src/shared/api'),
+        '@lib': resolve(__dirname, '../src/shared/lib'),
+        '@utils': resolve(__dirname, '../src/shared/utils')
+      }
+    }
+  });
 });
