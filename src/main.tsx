659d9ea41fc953ae8ef4a1ea23119fc287b06697
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2024-07-26 01:09:52 -0700

    feat: implement rqueryclient

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
-import { App } from '@app/app';
+import { App } from '@app/App';
+import { queryClient } from '@lib/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
