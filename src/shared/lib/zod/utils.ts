378044ef0c501187c318372cc31054195c26dcf6
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2024-07-29 14:09:29 -0700

    docs: add source for util function

--- src/shared/lib/zod/utils.ts
+++ src/shared/lib/zod/utils.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
+/*
+Referenced:
+ https://laniewski.me/blog/2023-11-19-api-response-validation-with-zod/#:~:text=How%20to%20use%20Zod,-In%20a%20real&text=This%20helper%20function%20takes%20a,message%20and%20throws%20an%20error.
+*/
 type ValidateApiConfig<TSchema extends z.ZodTypeAny> = {
   dto: unknown;
   schema: TSchema;
